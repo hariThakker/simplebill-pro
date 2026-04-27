3c6bb39687b837e8141a2e896ac094993732184a
HariThakker <vadanthakker@gmail.com>
2026-04-27 14:29:21 +0530

    fix: update supabase client with valid anon key

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://qblpecdpudamykvgqalx.supabase.co';
-const supabaseKey = 'sb_publishable_tlMLvbA4DsJnIrXWVMVbqA_KuSKdjel';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFibHBlY2RwdWRhbXlrdmdxYWx4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1MDA4MDIsImV4cCI6MjA5MjA3NjgwMn0.6Xpj4lRB6NHZ9w0o7bR-N3wosFrfFU0U5m-hHU-mDtE';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
